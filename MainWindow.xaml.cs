81798242e8c2d2fee6638b91f2b230d064cf1204
PerssonBrown <PerssonBrown@outlook.com>
2022-03-05 22:57:14 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Panuon.UI.Silver;
+
+namespace CleanLauncher
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : WindowX
+    {
+        // 链接 main_page
+        main_page.main_page main_page = new main_page.main_page();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResizeMode = System.Windows.ResizeMode.CanMinimize; // 禁止最大化
+        }
+
+        private void MainPage_Click(object sender, RoutedEventArgs e)
+        {
+            CtrlWindow.Content = new Frame()
+            {
+                Content = main_page
+            };
+        }
+    }
+}
---
+++ main_page/main_page.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CleanLauncher.main_page
+{
+    /// <summary>
+    /// main_page.xaml 的交互逻辑
+    /// </summary>
+    public partial class main_page : Page
+    {
+        // 绑定 offline
+        CleanLauncher.main_page.LoginUI.offline offline = new LoginUI.offline();
+        // 绑定 mojang
+        CleanLauncher.main_page.LoginUI.mojang mojang = new LoginUI.mojang();
+        public main_page()
+        {
+            InitializeComponent();
+        }
+
+        private void Offline_Click(object sender, RoutedEventArgs e)
+        {
+            login.Content = new Frame
+            {
+                Content = offline
+            };
+        }
+
+        private void Mojang_Click(object sender, RoutedEventArgs e)
+        {
+            login.Content = new Frame
+            {
+                Content = mojang
+            };
+        }
+    }
+}
